d811cb03b9c78684a4ccab71e4a823f4ac16728b
Lucas Lara <lucasml@live.com>
2021-09-15 18:26:22 -0300

    adding .env variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
     "username": "postgres",
@@ -14,10 +16,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.username,
+    "password": process.env.password,
+    "database": process.env.database,
+    "host": process.env.host,
+    "dialect": process.env.dialect,
+    "port": process.env.port,
   }
 }
